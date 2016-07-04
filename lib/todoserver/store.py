a320772a428b4a21f551fbae75b615df28e98160
John Kigozi <jkigozi@gmail.com>
2016-07-04 06:37:25 -0700

    separate taskstore interface

---
+++ lib/todoserver/store.py
@@ -0,0 +1,28 @@
+
+class TaskStore:
+    def __init__(self):
+        self.tasks = {}
+    
+    def get_all_tasks(self):
+        return [ {"id": task_id, "summary": task["summary"]}
+                for task_id, task in self.tasks.items()]
+        
+    def create_task(self, summary, description):
+        try:
+            self.task_id = 1 + max(self.tasks.keys())                    # don't do this in prod----not thread-safe at all
+        except ValueError:
+            task_id = 1
+        self.tasks[task_id] = {
+                            "summary": summary,
+                            "description": description
+                            }
+        
+        return task_id
+    
+    def task_details(self, task_id):
+        task_info = self.tasks[task_id]
+        task_info["id"] = task_id
+        return task_info
+    
+    def clear(self):
+        self.tasks.clear()
